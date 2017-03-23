0068861e64a79b6f9b8086f57149c78d10a7dc09
Jeff Shen <jshen@protonmail.ch>
2017-03-22 22:43:36 -0700

    Initial commit

---
+++ src/Body.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Body extends Component {
+    render() {
+        return (
+            <div className='body'>
+                Hello! My name is Jeff and this is a page created with React. Above are some buttons that can be pressed, although they do not lead anywhere.
+
+            </div>
+        );
+    }
+}
